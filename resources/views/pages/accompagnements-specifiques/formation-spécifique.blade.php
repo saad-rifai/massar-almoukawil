80c2ce047bb66186833942572315983783a2e1e3
MARIAM-BOUZINE <114150595+MARIAM-BOUZINE@users.noreply.github.com>
2022-09-22 16:44:38 +0100

    .

---
+++ resources/views/pages/accompagnements-specifiques/formation-spécifique.blade.php
@@ -0,0 +1,17 @@
+@section('title', 'Accompagnement digital')
+@include('assets.head')
+
+<body>
+    <div class="container-lg">
+        @include('assets.navbar')
+
+        <div class="body_card mt-5 mb-5">
+            <div class="container-body ">
+                
+                <br>
+            </div>
+        </div>
+        <br>
+
+        <hr>
+        @include('assets.footer')
